e0cd989ae632246e6b7ca375ee4efe636d3c6dc6
Andres <andresyovanne@gmail.com>
2024-01-12 15:07:03 -0300

    commit

--- Market/urls.py
+++ Market/urls.py
@@ -21,5 +21,6 @@ from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index),
+    path('', views.index, name='index'),
+    path('contact/', views.contact, name='contact'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
+from item.models import Category, Item 
 
 def index(requets):
+    items = Item.objects.filter (is_sold=False)
     return render(requets, 'index.html')
+
+def contact (requets):
+    return render(requets,'contact.html')
