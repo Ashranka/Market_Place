2f3c7c46144367ca8b6f628057fb06fee08c284a
Andres Shranka <andresyovanne@gmail.com>
2024-03-11 13:39:26 -0300

    signup y login

--- core/views.py
+++ core/views.py
@@ -1,9 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from item.models import Category, Item 
+from .forms import SignupForm
 
 def index(requets):
-    items = Item.objects.filter (is_sold=False)
-    return render(requets, 'index.html')
+    items = Item.objects.filter (is_sold=False)[0:6]
+    categories =Category.objects.all()
+    return render(requets, 'index.html',{
+        'categories': categories,
+        'items': items,
+    })
 
 def contact (requets):
     return render(requets,'contact.html')
+
+def singup (requets):
+    if requets.method == 'POST':
+        form = SignupForm(requets.POST)
+        
+        if form.is_valid():
+            form.save()
+            return redirect('/login/')
+    else:
+        form = SignupForm()
+    return render(requets, 'signup.html',{
+        'form': form
+    })
--- item/views.py
+++ item/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
+from item.models import Item
 
 # Create your views here.
+
+def detail(requets, pk):
+    item = get_object_or_404(Item,pk =pk)
+    related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(pk=pk)[0:3]
+
+    return render(requets, 'details.html',{
+        'item': item,
+        'related_items': related_items
+
+    })
